c3f7881148d74c3db24644d22a966b92cb49dcff
Leonel Zorzoli <63476706+LeoZorzoli@users.noreply.github.com>
2020-07-24 17:39:45 -0300

    Add files via upload

--- wiki/settings.py
+++ wiki/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+django_heroku.settings(locals())
